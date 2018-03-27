67e8cef35353a31bde689260af3bc8c6f054aec0
molon <dudl@qq.com>
2018-03-28 01:01:11 +0800

    batchCreate优化、支持批量不同行为、支持自动塞入插入和更新时间

--- callback_batch_create.go
+++ callback_batch_create.go
@@ -10,135 +10,151 @@ import (
 // Define callbacks for batch creating
 func init() {
 	DefaultCallback.BatchCreate().Register("gorm:begin_transaction", beginTransactionCallback)
+	DefaultCallback.BatchCreate().Register("gorm:update_time_stamp", updateTimeStampForBatchCreateCallback)
 	DefaultCallback.BatchCreate().Register("gorm:batch_create", batchCreateCallback)
 	DefaultCallback.BatchCreate().Register("gorm:commit_or_rollback_transaction", commitOrRollbackTransactionCallback)
 }
 
-// batchCreateCallback the callback used to insert data into database
-func batchCreateCallback(scope *Scope) {
-	value := scope.IndirectValue()
+// updateTimeStampForBatchCreateCallback will set `CreatedAt`, `UpdatedAt` when creating
+func updateTimeStampForBatchCreateCallback(scope *Scope) {
+	if !scope.HasError() {
+		now := NowFunc()
 
-	if value.Kind() != reflect.Slice {
-		scope.Err(fmt.Errorf("batchCreateCallback cannot be called for non-slice value, %+v given", value.Interface()))
-		return
-	}
+		indirectScopeValue := scope.IndirectValue()
 
-	var (
-		columns                      []string                        // one-dimensional array of strings containing columns
-		blankColumnsWithDefaultValue []string                        // one-dimensional array of strings containing columns
-		placeholders                 = make([][]string, value.Len()) // two-dimensional array of strings containing value placeholders
-		structFields                 = scope.GetModelStruct().StructFields
-	)
+		// 挨个元素去检查，为空则给予值
+		for elementIndex := 0; elementIndex < indirectScopeValue.Len(); elementIndex++ {
+			fields := filedsWithIndexForBatch(scope, elementIndex)
+			for _, field := range fields {
+				if !field.IsBlank {
+					continue
+				}
 
-	// Filling up the columns
-	for _, field := range fields(scope) {
-		// We don't treat non-normal fields on batch operations (relationships, etc)
-		if !field.IsNormal || field.IsIgnored {
-			continue
-		}
-
-		if field.IsBlank && field.HasDefaultValue {
-			blankColumnsWithDefaultValue = append(blankColumnsWithDefaultValue, scope.Quote(field.DBName))
-			scope.InstanceSet("gorm:blank_columns_with_default_value", blankColumnsWithDefaultValue)
-		} else if !field.IsPrimaryKey || !field.IsBlank {
-			columns = append(columns, scope.Quote(field.DBName))
-		}
-	}
-
-	// Filling up the placeholders
-	for elementIndex := 0; elementIndex < value.Len(); elementIndex++ {
-		valuePlaceholders := []string{}
-
-		for _, structField := range structFields {
-			// When inserting, the primary key is usually auto-increment
-			if !structField.IsPrimaryKey && !structField.IsIgnored {
-				fieldValue := reflect.Indirect(value.Index(elementIndex)).FieldByName(structField.Names[0]).Interface()
-				valuePlaceholders = append(valuePlaceholders, scope.AddToVars(fieldValue))
-			}
-		}
-
-		placeholders[elementIndex] = valuePlaceholders
-	}
-
-	var (
-		returningColumn = "*"
-		quotedTableName = scope.QuotedTableName()
-		primaryField    = scope.PrimaryField()
-		extraOption     string
-	)
-
-	if str, ok := scope.Get("gorm:insert_option"); ok {
-		extraOption = fmt.Sprint(str)
-	}
-
-	if primaryField != nil {
-		returningColumn = scope.Quote(primaryField.DBName)
-	}
-
-	lastInsertIDReturningSuffix := scope.Dialect().LastInsertIDReturningSuffix(quotedTableName, returningColumn)
-
-	scope.Raw(fmt.Sprintf(
-		"INSERT INTO %v (%v) VALUES %v%v%v",
-		scope.QuotedTableName(),
-		strings.Join(columns, ","),
-		strings.Join(joinValuePlaceholders(placeholders), ","),
-		addExtraSpaceIfExist(extraOption),
-		addExtraSpaceIfExist(lastInsertIDReturningSuffix),
-	))
-
-	// Executing the query
-	// TODO(drgomesp): Do we really need this check?
-	if lastInsertIDReturningSuffix == "" || primaryField == nil {
-		if result, err := scope.SQLDB().Exec(scope.SQL, scope.SQLVars...); scope.Err(err) == nil {
-			// set rows affected count
-			scope.db.RowsAffected, _ = result.RowsAffected()
-
-			if firstInsertedID, err := result.LastInsertId(); scope.Err(err) == nil {
-				fillPrimaryKeys(structFields, firstInsertedID, &value)
-			}
-		}
-	}
-}
-
-func fillPrimaryKeys(structFields []*StructField, firstInsertedID int64, values *reflect.Value) {
-	for _, structField := range structFields {
-		for i := 0; i < values.Len(); i++ {
-			field := reflect.Indirect(values.Index(i)).FieldByName(structField.Names[0])
-
-			if field.IsValid() && field.CanSet() {
-				if field.Kind() == reflect.Int64 || field.Kind() == reflect.Int32 || field.Kind() == reflect.Int8 || field.Kind() == reflect.Int {
-					id := firstInsertedID + int64(i)
-
-					if !field.OverflowInt(id) {
-						field.SetInt(id)
-					}
+				if field.Name == "CreatedAt" ||
+					field.Name == "UpdatedAt" {
+					field.Set(now)
 				}
 			}
 		}
 	}
 }
 
-func joinValuePlaceholders(placeholders [][]string) []string {
-	var valuePlaceholders []string
+// batchCreateCallback the callback used to insert data into database
+func batchCreateCallback(scope *Scope) {
+	if !scope.HasError() {
+		defer scope.trace(NowFunc())
 
-	for _, placeholder := range placeholders {
-		valuePlaceholders = append(valuePlaceholders, fmt.Sprintf("(%s)", strings.Join(placeholder, ",")))
+		indirectScopeValue := scope.IndirectValue()
+		if indirectScopeValue.Kind() != reflect.Slice {
+			scope.Err(fmt.Errorf("batchCreateCallback cannot be called for non-slice value, %+v given", indirectScopeValue.Interface()))
+			return
+		}
+
+		if indirectScopeValue.Len() <= 0 {
+			scope.Err(fmt.Errorf("batchCreateCallback cannot be called for empty slice, %+v given", indirectScopeValue.Interface()))
+			return
+		}
+
+		var (
+			columns      []string
+			placeholders = make([][]string, indirectScopeValue.Len())
+		)
+
+		// 列名获取
+		fields := filedsWithIndexForBatch(scope, 0)
+		existColumnNames := map[string]bool{}
+		for _, field := range fields {
+			if !field.IsNormal || field.IsIgnored {
+				continue
+			}
+
+			// 因为是批量，要支持各种情况，所以这里就简单的有效列名全都给予
+			columns = append(columns, scope.Quote(field.DBName))
+			existColumnNames[field.Name] = true
+		}
+
+		if len(columns) <= 0 {
+			scope.Err(fmt.Errorf("batchCreateCallback cannot be called for empty columns, %+v given", indirectScopeValue.Interface()))
+			return
+		}
+
+		// 塞入内容，因为是数组，所以需要挨个去塞
+		for elementIndex := 0; elementIndex < indirectScopeValue.Len(); elementIndex++ {
+			valuePlaceholders := []string{}
+
+			fields := filedsWithIndexForBatch(scope, elementIndex)
+			for _, field := range fields {
+				if existColumnNames[field.Name] {
+					var v interface{}
+					if !field.IsBlank {
+						v = field.Field.Interface()
+					} else {
+						// 如果field为空，但是有默认值，就直接塞入默认值即可
+						if field.HasDefaultValue {
+							v = field.TagSettings["DEFAULT"]
+						}
+						// 否则的话v就是nil嘛，然后最终会体现成NULL
+					}
+
+					valuePlaceholders = append(valuePlaceholders, scope.AddToVars(v))
+				}
+			}
+
+			placeholders[elementIndex] = valuePlaceholders
+		}
+
+		// 构造Values语句
+		valuePlaceholders := []string{}
+		for _, placeholder := range placeholders {
+			valuePlaceholders = append(valuePlaceholders, fmt.Sprintf("(%s)", strings.Join(placeholder, ",")))
+		}
+
+		// 额外Option
+		var extraOption string
+		if str, ok := scope.Get("gorm:insert_option"); ok {
+			extraOption = fmt.Sprint(str)
+		}
+
+		// 构造prepare语句
+		scope.Raw(fmt.Sprintf(
+			"INSERT INTO %v (%v) VALUES %v%v",
+			scope.QuotedTableName(),
+			strings.Join(columns, ","),
+			strings.Join(valuePlaceholders, ","),
+			addExtraSpaceIfExist(extraOption),
+		))
+
+		// 执行语句
+		if result, err := scope.SQLDB().Exec(scope.SQL, scope.SQLVars...); scope.Err(err) == nil {
+			scope.db.RowsAffected, _ = result.RowsAffected()
+
+			// TODO: 因为mysql底层driver不支持批量插入拿取最终insert id 所以这里也就没办法回写了，暂时没招
+		}
 	}
-
-	return valuePlaceholders
 }
 
-func fields(scope *Scope) []*Field {
-	var (
-		indirectScopeValue = scope.IndirectValue()
-		structFields       = scope.GetModelStruct().StructFields
-		fields             = make([]*Field, len(structFields))
-	)
-
-	for i, structField := range structFields {
-		fieldValue := reflect.Indirect(indirectScopeValue.Index(0)).FieldByName(structField.Names[0])
-		fields[i] = &Field{StructField: structField, Field: fieldValue, IsBlank: isBlank(fieldValue)}
+func filedsWithIndexForBatch(scope *Scope, index int) []*Field {
+	indirectScopeValue := scope.IndirectValue()
+	if index >= indirectScopeValue.Len() { // 不能越界
+		return nil
 	}
+	indirectScopeValue = reflect.Indirect(indirectScopeValue.Index(index))
 
+	isStruct := indirectScopeValue.Kind() == reflect.Struct
+	fields := []*Field{}
+	for _, structField := range scope.GetModelStruct().StructFields {
+		if isStruct {
+			fieldValue := indirectScopeValue
+			for _, name := range structField.Names {
+				if fieldValue.Kind() == reflect.Ptr && fieldValue.IsNil() {
+					fieldValue.Set(reflect.New(fieldValue.Type().Elem()))
+				}
+				fieldValue = reflect.Indirect(fieldValue).FieldByName(name)
+			}
+			fields = append(fields, &Field{StructField: structField, Field: fieldValue, IsBlank: isBlank(fieldValue)})
+		} else {
+			fields = append(fields, &Field{StructField: structField, IsBlank: true})
+		}
+	}
 	return fields
 }
